9ad56af9e1383217b84d8e0d41ca7793a1df64e8
Obssa <obssa.ahmed@torontomu.ca>
2023-02-26 22:03:52 -0500

    Filling out the urls_index.ejs template

--- express_server.js
+++ express_server.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
-
-const express = require("express");
-const app = express();
-const PORT = 8080; // default port 8080
 
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,4 +21,9 @@ app.get("/urls.json", (req, res) => {
 });
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+    const templateVars = {urls: urlDatabase};
+    res.render("urls_index", templateVars);
 });
