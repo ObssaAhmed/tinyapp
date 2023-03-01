a39889ff3e736838f1c9e829d8a297f5d20681a6
Obssa <obssa.ahmed@torontomu.ca>
2023-02-28 21:54:18 -0500

    Registering New Users

--- express_server.js
+++ express_server.js
@@ -5,6 +5,20 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const users = {
+    userRandomID: {
+        id: "userRandomID",
+        email: "user@example.com",
+        password: "purple-monkey-dinosaur",
+    },
+    user2RandomID: {
+        id: "user2RandomID",
+        email: "user2@example.com",
+        password: "dishwasher-funk",
+    },
+};
+
+
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
@@ -26,19 +40,26 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-    const templateVars = {urls: urlDatabase, username: req.cookies.username};
+    const userId = req.cookies.user_id;
+    const user = users[userId];
+    const templateVars = {urls: urlDatabase, user};
     res.render("urls_index", templateVars);
 });
 
+
 app.get("/urls/new", (req, res) => {
-    const templateVars = {username: req.cookies.username};
+    const userId = req.cookies.user_id;
+    const user = users[userId];
+    const templateVars = {user};
     res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
+    const userId = req.cookies.user_id;
+    const user = users[userId];
     const shortURL = req.params.shortURL;
-    const longURL = urlDatabase[shortURL];
-    const templateVars = {shortURL, longURL, username: req.cookies.username};
+    const longURL = urlDatabase[shortURL].longURL;
+    const templateVars = {shortURL, longURL, user};
     res.render("urls_show", templateVars);
 });
 
@@ -84,6 +105,11 @@ app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/register.html');
+});
+
+
 function generateRandomString() {
     const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let result = '';
@@ -92,3 +118,13 @@ function generateRandomString() {
     }
     return result;
 }
+
+app.post("/register", (req, res) => {
+    const {email, password} = req.body;
+    const id = generateRandomString();
+    const newUser = {id, email, password};
+    users[id] = newUser;
+    res.cookie("user_id", id);
+    res.redirect("/urls");
+});
+
